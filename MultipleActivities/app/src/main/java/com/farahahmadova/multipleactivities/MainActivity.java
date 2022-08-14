50c4c1aeb818c41429f98609a992b634b2a70a98
Farah <ahmadovafarah083@gmail.com>
2022-08-14 17:52:38 +0400

    OOP

---
+++ MultipleActivities/app/src/main/java/com/farahahmadova/multipleactivities/MainActivity.java
@@ -0,0 +1,26 @@
+package com.farahahmadova.multipleactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+String username;
+EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         username="";
+         editText=findViewById(R.id.editTextTextPersonName);
+    }
+    public void changeActivity (View view){
+        username=editText.getText().toString();
+        Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+        intent.putExtra("userInput",username);
+        startActivity(intent);
+    }
+}
